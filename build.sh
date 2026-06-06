79c4b1d9b5fe5c12e695490dbb8cdad5a7e1cc2a
Badhon010 <badhonemail001@gmail.com>
2026-06-06 19:22:09 +0600

    Css and Build fixes

--- build.sh
+++ build.sh
@@ -4,3 +4,13 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate --noinput
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.filter(username='admin').exists() or User.objects.create_superuser(
+    'admin',
+    'admin@example.com',
+    'Admin12@!'
+)
+"
